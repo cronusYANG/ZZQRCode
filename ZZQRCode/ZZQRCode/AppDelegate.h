3663d1745b4e6119c9195212274988785418575e
杨树彬 <yangshubin@wiseweb.com.cn>
2017-06-06 14:54:01 +0800

    创建仓库

---
+++ ZZQRCode/ZZQRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZQRCode
+//
+//  Created by POPLAR on 2017/6/6.
+//  Copyright © 2017年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZZQRCode/ZZQRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZZQRCode
+//
+//  Created by POPLAR on 2017/6/6.
+//  Copyright © 2017年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
